34b0aae05f2746bb1cbf63fdfe8f3965a76c7bed
ericLemanissier <ericLemanissier@users.noreply.github.com>
2020-08-07 09:41:44 +0200

    migrate glib to CCI

--- conanfile.py
+++ conanfile.py
@@ -55,7 +55,7 @@ class CairoConan(ConanFile):
             if self.options.enable_xlib or self.options.enable_xlib_xrender or self.options.enable_xcb:
                 self.requires("xorg/system")
         if self.options.enable_glib:
-            self.requires("glib/2.64.0@bincrafters/stable")
+            self.requires("glib/2.65.0")
         self.requires("zlib/1.2.11")
         self.requires("pixman/0.40.0")
         self.requires("libpng/1.6.37")
