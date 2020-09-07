9cddaab007f809e03318203c27d54ee54ab36095
ericLemanissier <ericLemanissier@users.noreply.github.com>
2020-09-07 23:33:11 +0200

    remove dead code

--- conanfile.py
+++ conanfile.py
@@ -158,13 +158,6 @@ class CairoConan(ConanFile):
         self._make_pkg_config()
 
     def _build_configure(self):
-        for package in self.deps_cpp_info.deps:
-            lib_path = self.deps_cpp_info[package].rootpath
-            for dirpath, _, filenames in os.walk(lib_path):
-                for filename in filenames:
-                    if filename.endswith('.pc'):
-                        shutil.copyfile(os.path.join(dirpath, filename), filename)
-                        tools.replace_prefix_in_pc_file(filename, lib_path)
         with tools.chdir(self._source_subfolder):
             # disable build of test suite
             tools.replace_in_file(os.path.join('test', 'Makefile.am'), 'noinst_PROGRAMS = cairo-test-suite$(EXEEXT)',
